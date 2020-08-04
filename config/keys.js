2cfc6ac3991ef2cf66467d8ac7ca777109552610
Petar Velkov <ppvelkov@gmail.com>
2020-08-04 18:03:05 +0300

    Profile API Routes

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://ClusterChef:DHqMd5JFTjAxKZj@thedatacluster.e8bls.mongodb.net/<TheDataCluster>?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://ClusterChef:DHqMd5JFTjAxKZj@thedatacluster.e8bls.mongodb.net/<TheDataCluster>?retryWrites=true&w=majority',
+    secretOrKey: 'secret'
 };
